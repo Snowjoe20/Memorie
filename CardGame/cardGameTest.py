f3b63ba67a7937458738a10fc4d76b3e94433152
Snowjoe20 <36928522+Snowjoe20@users.noreply.github.com>
2018-05-11 10:51:34 -0500

    Add files via upload
    need help seeing if the cards faces are the same to be able to undraw them.

---
+++ CardGame/cardGameTest.py
@@ -0,0 +1,36 @@
+from cardgame import *
+import random
+
+
+def main():
+
+    win = GraphWin("CardGame", 1500,800)
+    imageNames = ("ace.png", "CardFaces.png", "face02.png", "face03.png")
+    imageIndex = 0
+    cardList = []
+    for x in range(5, 400, 110):
+        cardList.append(Card(x,10,imageNames[imageIndex]))
+        imageIndex += 1
+    imageNames = ("CardFaces.png", "ace.png", "face03.png", "face02.png")
+    imageIndex = 0
+    for x in range(5, 400, 110):
+        cardList.append(Card(x, 210,imageNames[imageIndex]))
+        imageIndex += 1
+
+    for card in cardList:
+        card.drawCard(win)
+    while not win.closed:
+        clicked = win.getMouse()
+        for card in cardList:
+            card.flipCard(clicked, win)
+
+
+
+
+
+
+
+
+main()
+
+
