5f87da498922f62ec00c30fb84b31e445dcedc20
Snowjoe20 <36928522+Snowjoe20@users.noreply.github.com>
2018-05-18 10:57:46 -0500

    Add files via upload

--- CardGame/cardGameTest.py
+++ CardGame/cardGameTest.py
@@ -2,31 +2,56 @@ from cardgame import *
 import random
 
 
-def main():
+def main(self):
 
     win = GraphWin("CardGame", 1500,800)
+
+
+
     imageNames = ("ace.png", "CardFaces.png", "face02.png", "face03.png")
     imageIndex = 0
     cardList = []
     for x in range(5, 400, 110):
-        cardList.append(Card(x,10,imageNames[imageIndex]))
+        cardList.append(Card(x,10,imageNames[imageIndex], imageIndex))   ## creats a row of cards, but its saveing images to the cards drawn.
         imageIndex += 1
+
+
+
     imageNames = ("CardFaces.png", "ace.png", "face03.png", "face02.png")
     imageIndex = 0
     for x in range(5, 400, 110):
-        cardList.append(Card(x, 210,imageNames[imageIndex]))
+        cardList.append(Card(x, 210,imageNames[imageIndex], imageIndex))
         imageIndex += 1
 
     for card in cardList:
         card.drawCard(win)
-    while not win.closed:
+
+
+
+
+
+
+    turn = False
+    while not turn:    ## the while loop allows you to be able to click two cards at a time.
         clicked = win.getMouse()
         for card in cardList:
             card.flipCard(clicked, win)
 
 
+        clicked = win.getMouse()
+        for card in cardList:
+            card.flipCard(clicked, win)
 
 
+    
+
+
+
+
+        print("End of Turn")
+
+    win.getMouse()
+    win.close()
 
 
 
--- CardGame/cardgame.py
+++ CardGame/cardgame.py
@@ -3,13 +3,14 @@ import random
 
 class Card(object):
     "this creates the card"
-    def __init__(self,x,y,whatImage:str):
+    def __init__(self,x,y,whatImage:str, id):
         self.x= x
         self.y= y
         self.card = RoundedRectangle(Point(x,y),Point(x+100,y+150))
         self.card.setFill("red")
         self.cardImage = Image(Point(x + 50, y + 75), whatImage)
-        self.life = 0
+        self.ID = id
+        self.isFilp = False
 
 
 
@@ -18,6 +19,7 @@ class Card(object):
 
 
     def drawCard(self, win):
+        """draws card"""
         self.card.draw(win)
         self.card.setActiveFill("blue")
 
@@ -28,6 +30,7 @@ class Card(object):
         if clicked.getX()>= self.x and clicked.getX()<= self.x +100 and clicked.getY()>=self.y and clicked.getY()<self.y + 150:  #finds if the user cliked inside the object to undraw and than draw new face
             self.card.undraw()
             self.cardImage.draw(win)
+            self.isFlip = True
 
 
 
@@ -38,8 +41,11 @@ class Card(object):
 
 
     def unDrawCard(self,win):
-        self.card.undraw()
+        """undraws the image"""
         self.cardImage.undraw()
+
+
+
 
 
 
